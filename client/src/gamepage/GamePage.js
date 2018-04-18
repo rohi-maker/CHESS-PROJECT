aa92fdd0b3eca151750b4ed87a0c554f5865f100
Ngoc Dao <ngocdaothanh@gmail.com>
2018-04-18 16:20:42 +1000

    Display board at GamePage

--- client/src/gamepage/GamePage.js
+++ client/src/gamepage/GamePage.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react'
 import {Button, Col, Glyphicon, Jumbotron, Row} from 'react-bootstrap'
 import {Route} from 'react-router-dom'
 
+import Board from '../board/Board'
+
+const startingSEN = "r1n1bkqb1n1r/2p6p2/1prnbqkbnrp1/pppppppppppp/12/12/12/12/PPPPPPPPPPPP/1PRNBQKBNRP1/2P6P2/R1N1BKQB1N1R w KQkq KQkq - 0 0"
+
 export default class GamePage extends Component {
   static route = <Route exact path="/games/:gameId" component={GamePage}/>
 
@@ -10,8 +14,27 @@ export default class GamePage extends Component {
       <div>
         <Row>
           <Col md={8}>
+            <Board
+              ref={r => this.board =r}
+              sen={startingSEN}
+              lastMove=""
+
+              showCoords={true}
+              showLegalMoves={true}
+              showLastMove={true}
+              viewAsBlackPlayer={true}
+              allowMove={true}
+              onMove={this.onThisBoardMove.bind(this)}
+            />
+          </Col>
+
+          <Col md={4}>
             <Jumbotron>
-              Chessboard will be displayed here.
+              Player name and clock will be displayed here.
+            </Jumbotron>
+
+            <Jumbotron>
+              Player name and clock will be displayed here.
             </Jumbotron>
 
             <Button bsStyle="primary">
@@ -28,19 +51,12 @@ export default class GamePage extends Component {
               <Glyphicon glyph="education" />{' '}
               Hint
             </Button>
-          </Col>
 
-          <Col md={4}>
-            <Jumbotron>
-              Player name and clock will be displayed here.
-            </Jumbotron>
+            <br />
+            <br />
 
             <Jumbotron>
               Game moves will be displayed here.
-            </Jumbotron>
-
-            <Jumbotron>
-              Player name and clock will be displayed here.
             </Jumbotron>
           </Col>
         </Row>
@@ -52,5 +68,9 @@ export default class GamePage extends Component {
     const {boardOptions} = this.state
     boardOptions[key] = value
     this.setState({boardOptions})
+  }
+
+  onThisBoardMove(move) {
+
   }
 }
