413fc5da7638aac5ba1c61f174f2dfe08b1d4912
Ngoc Dao <ngocdaothanh@gmail.com>
2018-04-21 14:49:55 +1000

    Add TestBestMove

---
+++ engine/jvm/src/main/scala/synergychess/engine/TestBestMove.scala
@@ -0,0 +1,28 @@
+package synergychess.engine
+
+object TestBestMove {
+  def main(args: Array[String]) {
+    val game = GameGenerator.loadFromSEN(GameGenerator.startingSEN)
+
+    while (true) {
+      println(game.board.senString)
+
+      game.nextBestMove match {
+        case None =>
+          throw new RuntimeException("Could not get next best move")
+
+        case Some(moveData) =>
+          game.move(moveData) match {
+            case None =>
+              throw new RuntimeException("Could not play best move (invalid?)")
+
+            case Some(moveResult) =>
+              if (moveResult.mateData.trueCheckMate) {
+                println("True checkmate")
+                System.exit(0)
+              }
+          }
+      }
+    }
+  }
+}
