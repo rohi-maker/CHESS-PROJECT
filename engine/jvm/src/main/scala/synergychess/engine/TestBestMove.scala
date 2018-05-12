ccaa4743acf52c02600aea5f19d98e18e928eab3
Ngoc Dao <ngocdaothanh@gmail.com>
2018-05-12 10:12:14 +1000

    More log for TestBestMove

--- engine/jvm/src/main/scala/synergychess/engine/TestBestMove.scala
+++ engine/jvm/src/main/scala/synergychess/engine/TestBestMove.scala
@@ -2,27 +2,37 @@ package synergychess.engine
 
 object TestBestMove {
   def main(args: Array[String]) {
+    loopUntilGameOver()
+  }
+
+  private def loopUntilGameOver() {
     val game = GameGenerator.loadFromSEN(GameGenerator.startingSEN)
 
     while (true) {
-      println(game.board.senString)
+      println(game.senString)
 
       game.nextBestMove match {
         case None =>
           throw new RuntimeException("Could not get next best move")
 
         case Some(moveData) =>
+          println(s"Best move: $moveData")
+
           game.move(moveData) match {
             case None =>
               throw new RuntimeException("Could not play best move (invalid?)")
 
             case Some(moveResult) =>
-              if (moveResult.mateData.trueCheckMate) {
-                println("True checkmate")
+              val mateData = moveResult.mateData
+
+              if (mateData.trueCheckMate) {
+                println("True check mate")
                 System.exit(0)
-              } else if (moveResult.mateData.staleMate) {
-                println("Sheckmate")
+              } else if (mateData.staleMate) {
+                println("Stale mate")
                 System.exit(0)
+              } else if (mateData.checkMate) {
+                println("Check mate")
               }
           }
       }
