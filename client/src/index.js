f81953e195dea7af3d73f6ae15441cf60f78dc0c
Ngoc Dao <ngocdaothanh@gmail.com>
2018-05-13 10:54:22 +1000

    Improve alert UI about server connection problem

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Alert} from 'react-bootstrap'
 
 import App from './App'
 import serverUrl from './server'
 
 import './index.css'
 
-window.$debug = true
+window.$debug = (
+  process.env.NODE_ENV !== 'production' ||
+  window.location.search.indexOf('debug=true') >= 0
+)
+
+const serverAlert = (
+  <Alert bsStyle="warning">
+    <p>Server busy, please try again.</p>
+    <p><a href="">Reload.</a></p>
+  </Alert>
+)
 
 fetch(serverUrl('/whoami'), {credentials: 'include'})
   .then(res => res.json())
@@ -14,4 +25,6 @@ fetch(serverUrl('/whoami'), {credentials: 'include'})
     window.$me = me
     ReactDOM.render(<App />, document.getElementById('root'))
   })
-  .catch(e => alert('Server busy, please try again'))
+  .catch(e =>
+    ReactDOM.render(serverAlert, document.getElementById('root'))
+  )
