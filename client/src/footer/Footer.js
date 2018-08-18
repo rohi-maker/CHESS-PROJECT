ff55e3cc3dcad04d02920f1a0ef752a4604a9b56
Ngoc Dao <ngocdaothanh@gmail.com>
2018-08-18 10:13:24 +1000

    Add link to contact page

--- client/src/footer/Footer.js
+++ client/src/footer/Footer.js
@@ -13,6 +13,7 @@ export default class Footer extends Component {
             <li><Link to="/rules">Chess rules</Link></li>
             <li> | <Link to="/terms">Terms and conditions</Link></li>
             <li> | <a href="http://synergychess.net/">About</a></li>
+            <li> | <a href="http://synergychess.net/contact/">Contact</a></li>
           </ul>
 
           <p id="copyright">Copyright © 2018 Robert J Olsson. All rights reserved. No part of Synergy Chess may be reproduced, nor copied in any form, nor by any means, without the prior written permission of the above named person.</p>
