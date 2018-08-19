50c41600582c39ec7699a43765862f26ec357872
Ngoc Dao <ngocdaothanh@gmail.com>
2018-08-19 22:59:38 +1000

    Change let to const in client

--- client/src/board/Board.js
+++ client/src/board/Board.js
@@ -71,7 +71,7 @@ export default class Board extends Component {
     const moveData = new MoveData()
     const move = moveData.getDataFromString(moveString)
     const position = this.position
-    let [mateData, notation] = position.updatePositionFromString(moveString)
+    const [mateData, notation] = position.updatePositionFromString(moveString)
 
     // Check checkmate
     let kingRemoveChoices = []
