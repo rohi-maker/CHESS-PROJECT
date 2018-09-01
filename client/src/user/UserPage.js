a65384c86f0870c480b898aad39b4a762ed4145d
Ngoc Dao <ngocdaothanh@gmail.com>
2018-09-02 00:57:18 +1000

    Fix #49 List ended games a user has played at his profile page

--- client/src/user/UserPage.js
+++ client/src/user/UserPage.js
@@ -2,34 +2,59 @@ import React, {Component} from 'react'
 import {Col, Jumbotron, Row} from 'react-bootstrap'
 import {Link, Route} from 'react-router-dom'
 
+import EndedGames from '../gamelist/EndedGames'
 import {USER_GUEST} from '../userType'
+
+import serverUrl from '../server'
 
 export default class UserPage extends Component {
   static route = <Route exact path="/users/:username" component={UserPage}/>
 
+  constructor(props) {
+    super(props)
+    this.state = {point: undefined, rank: undefined, trend: undefined}
+  }
+
   render() {
+    const {username} = this.props.match.params
     const myPage =
-      window.$me.username === this.props.match.params.username &&
+      window.$me.username === username &&
       window.$me.userType !== USER_GUEST
+
+    const {point, rank, trend} = this.state
 
     return (
       <div>
         <Row>
           <Col md={5} sm={6}>
             <Jumbotron>
-              <p>User info will be displayed here.</p>
+              <p>{username}</p>
+
+              {point &&
+                <ul>
+                  <li>Point: {point}</li>
+                  <li>Rank: {rank}</li>
+                  <li>Trend: {trend}</li>
+                </ul>
+              }
 
               {myPage && <Link to="/settings/password">Settings</Link>}
             </Jumbotron>
           </Col>
 
           <Col md={7} sm={6}>
-            <Jumbotron>
-              <p>User games will be displayed here.</p>
-            </Jumbotron>
+            <EndedGames username={username} />
           </Col>
         </Row>
       </div>
     )
+  }
+
+  componentDidMount() {
+    const {username} = this.props.match.params
+    fetch(serverUrl(`/api/users/${username}`))
+      .then(res => res.json())
+      .then(ratings => this.setState(ratings['']))
+      .catch(e => {})
   }
 }
